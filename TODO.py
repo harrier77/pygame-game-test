20db127dfe626f6e0a6763c2f92f7fa267218bfe
harrier77 <harrier77@72631baa-75b6-8049-6d5a-face3817f41e>
2014-03-04 13:15:11 +0000

    eliminati file inutili

--- TODO.py
+++ TODO.py
@@ -1,8 +1,12 @@
+#-------------------------
 #- Cose da fare:
-#----------------
-# 1. Riscrivere la routine degli eventi in modo da gestire il caso del tasto tenuto premuto che non funziona
+#--------------------------
+#1. Riscrivere la routine degli eventi in modo da gestire il caso del tasto tenuto premuto che non funziona
 #2. Creare un cruscotto su cui scrivere le informazioni del gioco in corso
 #3. Riscrivere la routine del lancio dei proiettili che non funziona al di fuori di certa area
 #4. Aggiungere le musiche
-#5. Aggiungere una routine per leggere da un file di configurazione il punto della mappa in cui far entrare il giocatore
-#6. Scrivere una trama di base
+#5. Aggiungere una routine per leggere da un file di configurazione 
+	#5.1 il punto della mappa in cui far entrare il giocatore
+	#5.2 il livello della mappa su cui mettere il giocatore
+#6. Aggiungere la possibilità di attivare eventi sulla base dell'oggetto in collisione con il giocatore
+#7. Scrivere una trama di base
