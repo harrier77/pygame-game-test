8cc7ad8cb43110c58e1de0642efdd76fd27f020e
harrier77 <harrier77@gmail.com>
2014-03-25 10:10:14 +0000

    Aggiornamento

--- TODO.py
+++ TODO.py
@@ -1,12 +1,12 @@
 #-------------------------
 #- Cose da fare:
 #--------------------------
-#1. Riscrivere la routine degli eventi in modo da gestire il caso del tasto tenuto premuto che non funziona DA TESTARE
-#2. Creare un cruscotto su cui scrivere le informazioni del gioco in corso 	FATTO (in parte)
-#3. Riscrivere la routine del lancio dei proiettili che non funziona al di fuori di certa area
+#1. Riscrivere la routine degli eventi in modo da gestire il caso del tasto tenuto premuto che non funziona FATTO
+#2. Creare un cruscotto su cui scrivere le informazioni del gioco in corso 	SOSTITUITO CON TOOLKIT_HUD
+#3. Riscrivere la routine del lancio dei proiettili che non funziona al di fuori di certa area DA RIFARE
 #4. Aggiungere le musiche OK
-#5. Aggiungere una routine per leggere da un file di configurazione 
+#5. Aggiungere una routine per leggere da un file di configurazione DA RPENSARE PER USARE OBJECTS
 	#5.1 il punto della mappa in cui far entrare il giocatore
 	#5.2 il livello della mappa su cui mettere il giocatore
-#6. Aggiungere la possibilit‡ di attivare eventi sulla base dell'oggetto in collisione con il giocatore
-#7. Scrivere una trama di base
+#6. Aggiungere la possibilit√† di attivare eventi sulla base dell'oggetto in collisione con il giocatore TESTATO
+#7. Scrivere una trama di base FATTO
