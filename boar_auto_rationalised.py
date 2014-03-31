15c7530ff966f54f7d14416d859b8b6ce5e6e75c
harrier77 <harrier77@72631baa-75b6-8049-6d5a-face3817f41e>
2014-03-31 07:33:55 +0000

    'prova'

--- boar_auto_rationalised.py
+++ boar_auto_rationalised.py
@@ -1,3 +1,4 @@
+# coding: utf-8                     
 import sys
 sys.path.append(".\\miogummworld") 
 sys.path.append(".\\miogummworld\\gamelib") 
@@ -8,9 +9,11 @@ import sys
 import time
 from miefunzioni import pyganim
 import gummworld2
-from gummworld2 import geometry
+from gummworld2 import geometry,model
 import threading
 import datetime
+from pygame.sprite import Sprite
+from miefunzioni import miovar_dump
 
 def calcola_passi(or_pos=(300,200),target_pos=(200,200)):
 
@@ -22,24 +25,25 @@ def calcola_passi(or_pos=(300,200),target_pos=(200,200)):
         return lista_posizioni
 
 
-
-
-class Cinghiale():
+#inizio classe
+class Beast():
         def __init__(self):
                 # load the "standing" sprites (these are single images, not animations)
                 self.front_standing = pygame.image.load('animation/boar/boar_front_walk.001.gif')
                 self.back_standing = pygame.image.load('animation/boar/boar_back_walk.001.gif')
                 self.left_standing = pygame.image.load('animation/boar/boar_left_walk.001.gif')
                 self.right_standing = pygame.transform.flip(self.left_standing, True, False)
-
                 self.playerWidth, self.playerHeight = self.front_standing.get_size()
-
                 # creating the PygAnimation objects for walking/running in all directions
                 animTypes = 'left_walk back_walk front_walk NW SW'.split()
                 self.animObjs = {}
                 for animType in animTypes:
                     imagesAndDurations = [('animation/boar/boar_%s.%s.gif' % (animType, str(num).rjust(3, '0')), 0.1) for num in range(6)]
                     self.animObjs[animType] = pyganim.PygAnimation(imagesAndDurations)
+                my_aniType=['left_stand']
+                for animType in my_aniType:
+                    imagesAndDurations1 = [('animation/boar/boar_%s.%s.png' % (animType, str(num).rjust(3, '0')), 0.1) for num in range(12)]
+                    self.animObjs[animType] = pyganim.PygAnimation(imagesAndDurations1)
 
                 # create the right-facing sprites by copying and flipping the left-facing sprites
                 self.animObjs['right_walk'] = self.animObjs['left_walk'].getCopy()
@@ -51,76 +55,77 @@ class Cinghiale():
                 self.animObjs['NE'] = self.animObjs['NW'].getCopy()
                 self.animObjs['NE'].flip(True, False)
                 self.animObjs['NE'].makeTransformsPermanent()
-
-                #animObjs['right_run'] = animObjs['left_run'].getCopy()
-                #animObjs['right_run'].flip(True, False)
-                #animObjs['right_run'].makeTransformsPermanent()
-
-                # have the animation objects managed by a conductor.
-                # With the conductor, we can call play() and stop() on all the animtion
-                # objects at the same time, so that way they'll always be in sync with each
-                # other.
                 self.moveConductor = pyganim.PygConductor(self.animObjs)
+#fine classe -------------------------------------
 
-#Classe               
 
-class MovingBeast(object):
+#Inizio Classe               
+class MovingBeast(model.Object):
         
-        def __init__(self):
-            self.miocing=Cinghiale()
-            self.lista_destinazioni=[(400,400),(600,200),(500,300),(350,300)]
-            self.x=300
-            self.y=300
-            self.contatore_destinazioni=0
-            self.listap=[]
-            self.auto=False
-            self.lanciato=False
-            fotogramma=pygame.image.load('animation/boar/boar_left_walk.000.gif')
-            self.fotogramma=fotogramma
-        
+        def __init__(self,position=(100,100)):
+                model.Object.__init__(self)
+                self.miocing=Beast()
+                self.x=position[0]
+                self.y=position[1]
+                self.lista_destinazioni=self.comp_lista_destinazioni(self.x,self.y)
+                self.contatore_destinazioni=0
+                self.listap=[]
+                self.auto=False
+                self.lanciato=False
+                self.fotogramma=self.miocing.left_standing
+                self.miocing.moveConductor.play()
+                self.fotogramma=self.miocing.animObjs['left_stand'].ritorna_fotogramma()
+                
+                
+ 
+        def comp_lista_destinazioni(self,x,y):
+                lista_destinazioni=[]
+                x=x-50
+                y=y
+                lista_destinazioni.append((x,y))
+                y=y+50
+                lista_destinazioni.append((x,y))
+                x=x+150
+                lista_destinazioni.append((x,y))
+                x=x-100
+                y=y-100
+                lista_destinazioni.append((x,y))
+                return lista_destinazioni
+  
         def miotimer(self):
                 self.lanciato=True
-                self.auto=False
-                adesso= datetime.datetime.time(datetime.datetime.now())
-                print "timer per la pausa del cinghiale partito "+str(adesso)
+                self.auto=False  ##con auto=false il ciclo di muovi_cinghiale() non gira
+                #adesso= datetime.datetime.time(datetime.datetime.now())
+                #print "timer per la pausa del cinghiale partito "+str(adesso)
                 self.timer=threading.Timer(5.0, self.avvia)
                 self.timer.daemon=True  
                 self.timer.start() ##parte il timer che tiene fermo il cinghiale
 
-        def avvia(self):
-                self.auto=True
-                #self.lanciato=False
+        def avvia(self): ##questa procedura si avvia ogni n secondi e fa muovere il cinghiale e poi fermarsi
+                self.auto=True  ##con auto=True può girare il ciclo in muovi_cinghiale()
                 self.timer.cancel()
-                adesso= datetime.datetime.time(datetime.datetime.now())
-                print "finita pausa del cinghiale..."+str(adesso)
+                #adesso= datetime.datetime.time(datetime.datetime.now())
+                #print "finita pausa del cinghiale..."+str(adesso)
                 
                 
         def calcola_direzione(self,pos,mousepos):
             angolo= geometry.angle_of(pos,mousepos)
             ore=(angolo*100/360)*12
             if ore>100 and ore<200:
-                    #print 'NE'
                     direzione='NE'
             elif ore>400 and ore<500:
-                    #print "SE"
                     direzione='SE'
             elif ore>700 and ore<800:
-                    #print "SW"
                     direzione='SW'
             elif ore>1000 and ore<1100:
-                    #print "NW"
                     direzione='NW'
             elif ore>1100 or ore<100:
-                    #print "N"
                     direzione='back'
             elif ore>500 and ore<700:
-                    #print "S"
                     direzione='front'
             elif ore>200 and ore<400:
-                    #print "E"
                     direzione='right'
             elif ore>800 and ore<1000:
-                    #print "W"
                     direzione='left'
             return direzione
         
@@ -128,17 +133,16 @@ class MovingBeast(object):
         def scegli_fotogramma_animazione(self,miocing,direzione):
             if len(self.listap)>0:  ##inizia la camminata
                 pos= self.listap.pop(0)
-                self.x,self.y=pos
+                self.x,self.y=pos ##qui vengono impostati x e y del fotogramma da proiettare sullo schermo prendendoli dalla lista della camminata self.listap
                 miocing.moveConductor.play()
+                #di seguito viene selezionata l'iimmagine a seconda della direzione della camminata; x e y sono già stati impostati
                 if direzione=='left':
                     self.fotogramma=miocing.animObjs['left_walk'].ritorna_fotogramma()
                 elif direzione=='front':
                     self.fotogramma=miocing.animObjs['front_walk'].ritorna_fotogramma()
                 elif direzione=='right':
-                    #miocing.animObjs['right_walk'].blit(windowSurface, (x, y))
                     self.fotogramma=miocing.animObjs['right_walk'].ritorna_fotogramma()
                 elif direzione=='back':
-                    #miocing.animObjs['back_walk'].blit(windowSurface, (x, y))
                     self.fotogramma=miocing.animObjs['back_walk'].ritorna_fotogramma()
                 elif direzione=='SW':
                     self.fotogramma=miocing.animObjs['SW'].ritorna_fotogramma()
@@ -148,6 +152,7 @@ class MovingBeast(object):
                     self.fotogramma=miocing.animObjs['SE'].ritorna_fotogramma()
                 elif direzione=='NE':
                     self.fotogramma=miocing.animObjs['NE'].ritorna_fotogramma()
+                    
                 return self.fotogramma
         
         @property
@@ -157,74 +162,66 @@ class MovingBeast(object):
             else:
                 return False
         
-        def imposta_posizione_via_mouse(self,event):
-            if event.type==pygame.MOUSEBUTTONDOWN:
-                mousepos=pygame.mouse.get_pos()
-                self.direzione=self.calcola_direzione((self.x,self.y),mousepos)
-                self.listap=calcola_passi(or_pos=(self.x,self.y),target_pos=mousepos)
+        @property
+        def sprite_fotogramma(self):
+            fotog_sprite=pygame.sprite.Sprite()
+            fotog_sprite.image=self.fotogramma
+            fotog_sprite.rect=self.fotogramma.get_rect()
+            fotog_sprite.rect.x=self.x
+            fotog_sprite.rect.y=self.y
+            return fotog_sprite
         
+        @property
+        def rect(self):
+            rect=self.sprite_fotogramma.rect
+            return rect
+        
+        @property
+        def image(self):
+            image=self.fotogramma
+            return image
+        
+        #----------------------------------------
         def muovi_cinghiale(self):
             if self.auto: ##verifica se deve procedere a calcolare una nuova sequenza di passi
                         if self.contatore_destinazioni>len(self.lista_destinazioni)-1: self.contatore_destinazioni=0 ##resetta il contatore della lista delle destinazioni automatiche
-                        self.listap=calcola_passi(or_pos=(self.x,self.y),target_pos=self.lista_destinazioni[self.contatore_destinazioni])
-                        tpos=self.listap[len(self.listap)-1] ##calcola la direzione della destinazione da raggiungere
-                        self.direzione=self.calcola_direzione((self.x,self.y),tpos)
+                        self.listap=calcola_passi(or_pos=(self.x,self.y),target_pos=self.lista_destinazioni[self.contatore_destinazioni])  ##qui viene compilata la lista dei passi da seguire per camminare nel percorso
+                        pos_da_raggiungere=self.listap[len(self.listap)-1] ##legge la posizione finale di destinazione dalla lista delle destinazioni
+                        self.direzione=self.calcola_direzione((self.x,self.y),pos_da_raggiungere) ##calcola la direzione della destinazione da raggiungere
                         self.contatore_destinazioni=self.contatore_destinazioni+1 ##incrementa il contatore delle destinazioni
                         self.auto=False ##arresta il cinghiale quando ha fatto una singola camminata
-                        self.lanciato=False
+                        self.lanciato=False ##setta il timer su fermo mentre la camminata è in corso
+            
+            if self.is_walking: 
+                self.scegli_fotogramma_animazione(self.miocing,self.direzione)
+            else:
+                self.fotogramma=self.miocing.animObjs['left_stand'].ritorna_fotogramma()
 
-            if self.is_walking: self.scegli_fotogramma_animazione(self.miocing,self.direzione)
-            #print "lanciato "+str(self.lanciato)
-            #print "auto "+str(self.auto)
-            #print "is walking "+str(self.is_walking)
             if (self.lanciato==False) and (self.is_walking==False): 
                     self.miotimer()
+                    fotog_sprite=self.sprite_fotogramma
+            #print self.rect
             return self.fotogramma
-            
-            
-            
-        def main(self):
-                pygame.init()
-                screen = pygame.display.set_mode((800, 600), 0, 32)
-                pygame.display.set_caption('Cinghiale in movimento')
-                BGCOLOR = (180, 180, 180)
-                mainClock = pygame.time.Clock()
-                while True:
-                    screen.fill(BGCOLOR)
-                    fotogramma=self.muovi_cinghiale()
-                    screen.blit(fotogramma, (self.x, self.y))
-
-                    for event in pygame.event.get(): # event handling loop
-                        # handle ending the program
-                        if event.type == QUIT:
-                            pygame.quit()
-                            sys.exit()
-                        elif event.type == KEYDOWN:
-                            if event.key == K_ESCAPE:
-                                pygame.quit()
-                                sys.exit()
-                            if event.key == K_F4:
-                                self.auto=True
-                        elif event.type == KEYUP:
-                                pass
-                        self.imposta_posizione_via_mouse(event)
-                                        
-                    pygame.display.flip()
-                    mainClock.tick(50) # Feel free to experiment with any FPS setting.
-
+        #---------------------------------------------------
 #fine della Classe
+
+
+
+
                     
 def main():
     ogg=MovingBeast()
     pygame.init()
-    screen = pygame.display.set_mode((800, 600), 0, 32)
+    screen = pygame.display.set_mode((600, 300))
     pygame.display.set_caption('Cinghiale in movimento')
     BGCOLOR = (180, 180, 180)
     mainClock = pygame.time.Clock()
+    #print ogg.sprite_fotogramma.rect
     while True:
         screen.fill(BGCOLOR)
-        fotogramma=ogg.muovi_cinghiale()
-        screen.blit(fotogramma, (ogg.x, ogg.y))
+        ogg.muovi_cinghiale()
+
+        screen.blit(ogg.sprite_fotogramma.image, (ogg.rect.x, ogg.rect.y))
 
         for event in pygame.event.get(): # event handling loop
             # handle ending the program
@@ -235,9 +232,10 @@ def main():
                 if event.key == K_ESCAPE:
                     pygame.quit()
                     sys.exit()
-                            
+                if event.key == K_F4:
+                    print 'f4'
         pygame.display.flip()
-        mainClock.tick(50) # Feel free to experiment with any FPS setting.
+        mainClock.tick(40) # Feel free to experiment with any FPS setting.
               
                     
 if __name__ == '__main__':
